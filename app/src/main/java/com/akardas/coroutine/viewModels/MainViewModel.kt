a28da274b7c26a6a58ea34b7fd43e03051651304
akardas16 <krdsabdllh53@gmail.com>
2022-11-19 21:13:48 +0100

    shouldn't use runBlocking{}

--- app/src/main/java/com/akardas/coroutine/viewModels/MainViewModel.kt
+++ app/src/main/java/com/akardas/coroutine/viewModels/MainViewModel.kt
@@ -2,42 +2,70 @@ package com.akardas.coroutine.viewModels
 
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.akardas.coroutine.networking.Conclude
 import com.akardas.coroutine.networking.MainRepository
 import com.akardas.coroutine.networking.models.LoginDataModel
-import com.akardas.coroutine.networking.ConcludeData
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import com.akardas.coroutine.retrofit.Status
+import kotlinx.coroutines.*
 
 class MainViewModel(private val mainRepository: MainRepository) : ViewModel() {
 
-    fun getUsers() = runBlocking {
-        withContext(Dispatchers.IO){
-            ConcludeData.getResultData(mainRepository.getUsers())
-        }
-    }
-
-    fun registerUser(dataModel: LoginDataModel) = runBlocking {
-        withContext(Dispatchers.IO){
-            ConcludeData.getResultData(mainRepository.registerUser(dataModel))
-        }
-    }
-
-     fun getSingleUser(userID: Int) = runBlocking {
-         withContext(Dispatchers.IO) {
-             ConcludeData.getResultData(mainRepository.getSingleUser(userID))
+     fun getUsers() = liveData(Dispatchers.IO) {
+         emit(Conclude.loading(Status.START))
+         try {
+             emit(Conclude.loading(Status.FINISH))
+             emit(Conclude.success(mainRepository.getUsers()))
+         }catch (e:Exception){
+             emit(Conclude.loading(Status.FINISH))
+             emit(Conclude.error(e))
          }
      }
 
-    fun updateUser(dataModel: LoginDataModel,userID: Int) = runBlocking {
-        withContext(Dispatchers.IO){
-            ConcludeData.getResultData(mainRepository.updateUser(dataModel,userID))
+
+
+
+    fun registerUser(dataModel: LoginDataModel) = liveData(Dispatchers.IO) {
+        emit(Conclude.loading(Status.START))
+        try {
+            emit(Conclude.loading(Status.FINISH))
+            emit(Conclude.success(mainRepository.registerUser(dataModel)))
+        }catch (e:Exception){
+            emit(Conclude.loading(Status.FINISH))
+            emit(Conclude.error(e))
         }
     }
 
-    fun updateUserWithPath(dataModel: LoginDataModel) = runBlocking {
-        withContext(Dispatchers.IO){
-            ConcludeData.getResultData(mainRepository.updateUserWithPatch(dataModel))
+     fun getSingleUser(userID: Int) = liveData(Dispatchers.IO) {
+         emit(Conclude.loading(Status.START))
+         try {
+             emit(Conclude.loading(Status.FINISH))
+             emit(Conclude.success(mainRepository.getSingleUser(userID)))
+         }catch (e:Exception){
+             emit(Conclude.loading(Status.FINISH))
+             emit(Conclude.error(e))
+         }
+     }
+
+    fun updateUser(dataModel: LoginDataModel,userID: Int) = liveData(Dispatchers.IO) {
+        emit(Conclude.loading(Status.START))
+        try {
+            emit(Conclude.loading(Status.FINISH))
+            emit(Conclude.success(mainRepository.updateUser(dataModel, userID)))
+        }catch (e:Exception){
+            emit(Conclude.loading(Status.FINISH))
+            emit(Conclude.error(e))
+        }
+    }
+
+    fun updateUserWithPath(dataModel: LoginDataModel) = liveData(Dispatchers.IO) {
+        emit(Conclude.loading(Status.START))
+        try {
+            emit(Conclude.loading(Status.FINISH))
+            emit(Conclude.success(mainRepository.updateUserWithPatch(dataModel)))
+        }catch (e:Exception){
+            emit(Conclude.loading(Status.FINISH))
+            emit(Conclude.error(e))
         }
     }
 
